09ad57c8573312ec783f6465f42cea7ecef9e3c5
Glauber FC <gfc714@gmail.com>
2017-02-03 23:31:39 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,2 @@
+// Creating the module called parking
+var parking = angular.module("parking", ["ngAnimate"]);
---
+++ js/controllers.js
@@ -0,0 +1,17 @@
+// Registering the parkingCtrl to the parking module
+parking.controller("parkingCtrl", function ($scope) {
+	$scope.appTitle = "[Packt] Parking";
+	// Binding the car's array to the scope
+	$scope.cars = [];
+
+	$scope.colors = ["White", "Black", "Blue", "Red", "Silver"];
+
+	$scope.alertTopic = "Something went wrong!";
+	$scope.descriptionTopic = "You must inform the plate and the color of the car!";
+
+	// Binding the park function to the scope
+	$scope.park = function (car) {
+		$scope.cars.push(angular.copy(car));
+		delete $scope.car;
+	}
+});
